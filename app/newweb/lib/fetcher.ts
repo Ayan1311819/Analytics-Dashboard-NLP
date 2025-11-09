cec06109ddf3f0a85dbceccfa003f9d09bb01d3a
Ayan <ayanchoudhary040@gmail.com>
2025-11-10 02:55:22 +0530

    fix

--- app/newweb/lib/fetcher.ts
+++ app/newweb/lib/fetcher.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const base = process.env.NEXT_PUBLIC_API_BASE || "http://localhost:4000";
+const base = "https://a-backend-jhdl.onrender.com" ;
 
 const fetcher = async (url: string) => {
   const res = await axios.get(`${base}${url}`);
